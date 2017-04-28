5fb73312b162cb90e6d5e9adb65626a4b69d8af6
Tony Tran <tcbtran@T-MacBook-Pro.local>
2017-04-27 21:54:59 -0700

    Initial Submission

---
+++ server.js
@@ -0,0 +1,33 @@
+// Load our node dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Serve static content for the app from the "/public" directory in the app folder
+app.use(express.static(__dirname + "/public"));
+
+// Using BodyParser to interpret data sent to it
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json"}));
+
+// Friends data array
+var friends = [
+	{name: "William",
+	 photo: "",
+	 scores: []
+	},
+];
+
+// Routes
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// Initiate the listener
+app.listen(PORT, function() {
+	console.log("App listening on PORT:%s", PORT);
+});
